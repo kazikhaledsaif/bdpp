cedbf71ac1ba363d9c881c2f13e418379f192243
mdmuaj13 <iammuaj@gmail.com>
2019-04-27 09:01:37 +0600

    home page with localization

---
+++ resources/lang/en/home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hasan
+ * Date: 4/27/2019
+ * Time: 8:43 AM
+ */
+
+return [
+    'about_us' => 'About Us',
+    'about_detail' => 'Considering the spirit of the Liberation War, Bengali nationalism and non-communalism, the diploma engineers of independent Bangladesh, in association with the Father of the Nation Bangabandhu Sheikh Mujibur Rahman, who are studying in all professional diploma engineers and diploma engineering courses in the country, considering their own responsibility for the protection of their rights and the welfare of the people. Keep it The decision was taken to name the organization named "Bangabandhu Diploma Engineer Council". It will be run as a non-political social and research organization.',
+    'profile' => 'Founder ',
+    'pname' => 'Mujibur Rahman ',
+    'ptitle' => 'Engineer  ',
+    'pthird' => 'Social Organization',
+    'qoute' => 'Member of Parliament, Mayor of City Corporation, Deputy Mayor and Councilors, Gazetted Officers, Public University Teachers, Upazila Parishad Chairman and Vice Chairman, Municipality Mayor and Municipal Councilors',
+    'notice' => 'Notice Board',
+    'links' => 'Important Link',
+    'link1' => 'District Committee Form',
+    'link2' => 'Central Committee Form',
+    'link3' => 'Departmental Committee',
+    'locale' => 'en',
+];
